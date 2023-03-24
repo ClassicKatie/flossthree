9544b448ce4f7866a3778891be87edaef814e8b4
Katie Ford <katie@katieford.io>
2023-03-23 19:04:12 -0500

    feat: add new api endpoint, save to s3

---
+++ api/api.py
@@ -0,0 +1,20 @@
+import hug
+from PIL import Image
+import tempfile
+import urllib
+
+from pattern import Pattern
+
+@hug.local
+def post_make_pattern(image_url: str, pattern_name: str, user_ref: dict):
+    """
+    From an image, create a pattern
+    """
+    tmp = tempfile.mktemp()
+    urllib.request.urlretrieve(‘Image url’, tmp)
+    im = Image.open(tmp)
+
+    new_pattern = Pattern(im, 'dmcfloss')
+    new_pattern.build_and_save()
+
+    return {'chart': new_pattern.rendered_chart}
